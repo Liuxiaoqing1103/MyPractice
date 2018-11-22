f97389d49b9f4a8c49e408aea04c47642ce5754f
Liuxiaoqing <xq_liu1103@163.com>
2018-11-22 23:41:24 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyPractice',
+    version='',
+    packages=[''],
+    url='https://github.com/Liuxiaoqing1103/MyPractice.git',
+    license='',
+    author='Administrator',
+    author_email='',
+    description=''
+)
